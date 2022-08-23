05a43b4f9a3fa991c6f285cfc2f8787d0350bb6f
Victor <victor.fiamoncini@gmail.com>
2022-08-23 20:54:07 -0300

    first commit

---
+++ src/03.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  try {
+    stdout.write('Informe o salário: ');
+    final wage = num.parse(stdin.readLineSync().toString());
+
+    stdout.write('Informe o percentual de reajuste: ');
+    final readjustment_percentage = num.parse(stdin.readLineSync().toString());
+
+    final new_wage = wage + (wage / 100) * readjustment_percentage;
+
+    stdout.write('Salário com percentual de ajuste: $new_wage');
+  } catch (e) {
+    print('Ocorreu o seguinte erro -> $e');
+  }
+}
