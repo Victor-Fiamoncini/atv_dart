afe13f46192945c7b5dde79bb1046d204adddd2e
Victor <victor.fiamoncini@gmail.com>
2022-08-24 19:27:58 -0300

    feat: add atv 09

---
+++ src/09.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  try {
+    stdout.write('Informe o quanto você ganha por hora: ');
+    final hour_value = double.parse(stdin.readLineSync().toString());
+
+    stdout.write('Informe o número de horas trabalhadas por mês: ');
+    final hours_worked = double.parse(stdin.readLineSync().toString());
+
+    final total_salary = hour_value * hours_worked;
+    final irpf = (total_salary * 11) / 100;
+    final inss = (total_salary * 8) / 100;
+    final sindicate = (total_salary * 5) / 100;
+    final liquid_salary = total_salary - irpf - inss - sindicate;
+
+    print('Salário bruto: $total_salary');
+    print('IRPF: $irpf');
+    print('INSS: $inss');
+    print('Sindicato: $sindicate');
+    print('Salário líquido: $liquid_salary');
+  } on FormatException {
+    print('Erro ao formatar os valores digitados');
+  } catch (e) {
+    print('Ocorreu o seguinte erro -> $e');
+  }
+}
